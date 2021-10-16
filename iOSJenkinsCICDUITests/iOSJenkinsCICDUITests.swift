187aadba7e55b4ad139b24aab88016e9c9bb993b
Gabriel Silva <ice_gsilva@uolinc.com>
2021-10-16 01:37:12 -0300

    initial commit

---
+++ iOSJenkinsCICDUITests/iOSJenkinsCICDUITests.swift
@@ -0,0 +1,14 @@
+//
+//  iOSJenkinsCICDUITests.swift
+//  iOSJenkinsCICDUITests
+//
+//  Created by Gabriel Monteiro Camargo da Silva - GCM on 16/10/21.
+//
+
+import XCTest
+
+class iOSJenkinsCICDUITests: XCTestCase {
+    func testAssert() {
+        XCTAssertTrue(true)
+    }
+}
